d7a4189bbfd34a7e4b4ba4fd39b1ec46749e4188
Sina Dabiri <sdabiri@emory.edu>
2026-04-28 17:00:03 -0400

    re-running without dPCA, and updating cosine similarity plots.

--- fast_main_DR_v2.m
+++ fast_main_DR_v2.m
@@ -33,12 +33,12 @@ end
 %% Loop through experiments
 
 conditions = {'set4'}; %,'ou', 'set2',  linear, nonlinear
-nDatasets  = 1; % 10
-k_range    = 6:6; % 9
+nDatasets  = 10; % 10
+k_range    = 1:9; % 9
 nK         = numel(k_range);
 
 % Store results: structure indexed by method name
-methods = {'UMAP'}; % 'iVAE' 'PCA', 'AE','dPCA', 'ICA','UMAP' 
+methods = {'PCA', 'AE', 'ICA'}; % 'iVAE' 'PCA', 'AE','dPCA', 'ICA','UMAP' 
 
 EXP = struct();
 param = struct();
@@ -53,15 +53,15 @@ RESULTS.meta.description = "Dimensionality reduction benchmark";
 
 % Best practice: Leave 1-2 cores free for the OS/Main Thread.
 % For a 7-core system, 5 workers is a safe, high-performance choice.
-% target_workers = 5; 
-% current_pool = gcp('nocreate');
-% 
-% if isempty(current_pool)
-%     parpool(target_workers);
-% elseif current_pool.NumWorkers ~= target_workers
-%     delete(current_pool);
-%     parpool(target_workers);
-% end
+target_workers = 5; 
+current_pool = gcp('nocreate');
+
+if isempty(current_pool)
+    parpool(target_workers);
+elseif current_pool.NumWorkers ~= target_workers
+    delete(current_pool);
+    parpool(target_workers);
+end
 
 %% Loop through experiments
 for c = 1:numel(conditions)
@@ -74,7 +74,7 @@ for c = 1:numel(conditions)
     % ---------------------------------------------------------------------
     % PARALLEL LOOP
     % ---------------------------------------------------------------------
-    for d = 1:nDatasets
+    parfor d = 1:nDatasets
         fprintf('Dataset %d / %d (Worker Processing)\n', d, nDatasets);
         data = struct();
         
